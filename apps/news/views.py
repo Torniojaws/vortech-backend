d932432096a25e9674d69e45b0a820c292a58561
Juha Untinen <juha.untinen@leaddesk.com>
2017-09-03 18:06:49 +0000

    More Flask-Classful things

--- apps/news/views.py
+++ apps/news/views.py
@@ -1,25 +1,5 @@
 from flask_classful import FlaskView, route
 
-"""
-Note to self:
-
-For simple routes like
-GET /news
-POST /news
-GET /news/:id
-PATCH /news/:id
-UPDATE /news/:id
-DELETE /news/:id
-Using the standard Flask-Classful methods is OK. Just needs a special handling for GET so that both
-GET /news and GET /news/:id are supported.
-
-For "special" cases like:
-PATCH /news/:id:/comments/:id
-It is fine to use the standard
-@route("/<int:news_id>/comments/<int:comment_id>")
-style
-"""
-
 
 class NewsView(FlaskView):
     def index(self):
@@ -29,7 +9,7 @@ class NewsView(FlaskView):
         return "This is GET /news/{}\n".format(news_id)
 
     def post(self):
-        return "This is GET /news\n"
+        return "This is POST /news\n"
 
     def put(self, news_id):
         return "This is PUT /news/{}\n".format(news_id)
@@ -42,8 +22,8 @@ class NewsView(FlaskView):
 
     @route("/<int:news_id>/comments/<int:comment_id>", methods=["GET"])
     def news_comment(self, news_id, comment_id):
-        return "This is GET /news/{}/comments/{}".format(news_id, comment_id)
+        return "This is GET /news/{}/comments/{}\n".format(news_id, comment_id)
 
     @route("/<int:news_id>/comments/", methods=["GET"])
     def news_comments(self, news_id):
-        return "This is GET /news/{}/comments/".format(news_id)
+        return "This is GET /news/{}/comments/\n".format(news_id)

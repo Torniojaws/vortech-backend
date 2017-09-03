4cdc0c1f3026a3bbf852697528449a775182de58
Juha Untinen <juha.untinen@leaddesk.com>
2017-09-03 16:59:34 +0000

    More Flask-Classful things

--- apps/news/views.py
+++ apps/news/views.py
@@ -1,33 +1,49 @@
-from flask_classful import FlaskView
+from flask_classful import FlaskView, route
+
+"""
+Note to self:
+
+For simple routes like
+GET /news
+POST /news
+GET /news/:id
+PATCH /news/:id
+UPDATE /news/:id
+DELETE /news/:id
+Using the standard Flask-Classful methods is OK. Just needs a special handling for GET so that both
+GET /news and GET /news/:id are supported.
+
+For "special" cases like:
+PATCH /news/:id:/comments/:id
+It is fine to use the standard
+@route("/<int:news_id>/comments/<int:comment_id>")
+style
+"""
 
 
 class NewsView(FlaskView):
     def index(self):
-        return "News index"
+        return "This is GET /news\n"
 
     def get(self, news_id):
-        if news_id:
-            return "This is GET /news/{}".format(news_id)
-        else:
-            return "This is GET /news"
+        return "This is GET /news/{}\n".format(news_id)
 
     def post(self):
-        return "This is POST /news"
+        return "This is GET /news\n"
 
+    def put(self, news_id):
+        return "This is PUT /news/{}\n".format(news_id)
 
-"""
-TODO:
-/api/1.0/news/123/comments/321
+    def patch(self, news_id):
+        return "This is PATCH /news/{}\n".format(news_id)
 
-Seems to be like this:
-NewsView():
-    def something():
+    def delete(self, news_id):
+        return "This is DELETE /news/{}\n".format(news_id)
 
+    @route("/<int:news_id>/comments/<int:comment_id>", methods=["GET"])
+    def news_comment(self, news_id, comment_id):
+        return "This is GET /news/{}/comments/{}".format(news_id, comment_id)
 
-# SOmething like this in https://pythonhosted.org/Flask-Classful/
-class AnotherView(FlaskView):
-    route_base = "home"
-
-    def this_view(self, arg1, arg2):
-        return "Args: %s, %s" % (arg1, arg2,)
-"""
+    @route("/<int:news_id>/comments/", methods=["GET"])
+    def news_comments(self, news_id):
+        return "This is GET /news/{}/comments/".format(news_id)

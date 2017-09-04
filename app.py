f8c7d3895d852f4bc5d9759814ab63206b7ca9c6
Juha Untinen <torniojaws@gmail.com>
2017-09-05 02:55:01 +0300

    Apidoc route change

--- app.py
+++ app.py
@@ -16,19 +16,5 @@ register_models(app)
 # Register all views for Flask
 register_views(app)
 
-
-@app.route("/")
-@auto.doc()
-def index():
-    """This is the main entry point, ye landlubbers!"""
-    return "Land ahoy!"
-
-
-@app.route("/apidoc")
-def documentation():
-    """All routes will be documented automatically here"""
-    return auto.html()
-
-
 if __name__ == "__main__":
     app.run(host='0.0.0.0')

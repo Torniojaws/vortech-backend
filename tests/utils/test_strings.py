6854b741250ef2e0eca0515a752c3b6221f66b27
Juha Untinen <torniojaws@gmail.com>
2021-05-14 18:49:27 +0300

    Implement int_or_none for cleaner code

--- tests/utils/test_strings.py
+++ tests/utils/test_strings.py
@@ -1,6 +1,6 @@
 import unittest
 
-from apps.utils.strings import linux_linebreaks
+from apps.utils.strings import int_or_none, linux_linebreaks
 
 
 class TestStringsUtils(unittest.TestCase):
@@ -31,3 +31,23 @@ class TestStringsUtils(unittest.TestCase):
         """Just to be sure paragraph breaks (= 2 x Enter) are correct"""
         test = "Test\rString\r\n\r\nHere\n"
         self.assertEqual("Test\nString\n\nHere\n", linux_linebreaks(test))
+
+    def test_int_or_none_with_valid_values(self):
+        """Should return the value back as an int type value."""
+        self.assertEqual(True, isinstance(int_or_none(2), int))
+        self.assertEqual(3, int_or_none(3))
+        self.assertEqual(4, int_or_none("4"))
+        self.assertEqual(0, int_or_none("0"))
+
+    def test_int_or_none_with_invalid_possible_values(self):
+        """Should return None when value is possible but not an int."""
+        self.assertEqual(None, int_or_none("a2"))
+        self.assertEqual(None, int_or_none(""))
+        self.assertEqual(None, int_or_none("aaabbb"))
+        self.assertEqual(None, int_or_none("😀"))
+
+    def test_int_or_none_with_invalid_nonsense_values(self):
+        """Should return None for 'impossible' values."""
+        self.assertEqual(None, int_or_none(['lists', 'cannot', 'be', 'used', 'in', 'urlparams']))
+        self.assertEqual(None, int_or_none({"dict": "is also not possible in urlparams"}))
+        self.assertEqual(None, int_or_none(None))
